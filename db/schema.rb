4408338c9d8905ad236e5b19e2bc3982dd1dca21
BeatrizCSSantos <beatrizcs96@hotmail.com>
2023-01-14 15:00:23 -0300

    final config

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_11_122738) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_14_171625) do
+  create_table "accounts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.string "author"
@@ -31,5 +38,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_122738) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "accounts", "users"
   add_foreign_key "books", "users"
 end
